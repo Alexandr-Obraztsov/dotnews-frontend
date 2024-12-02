99e7e443eb1af2c7d59a46a76bdab463879c3c6c
alexandr-obraztsov <alexlove2005@mail.ru>
2024-12-02 15:13:11 +0300

    check errors

--- src/features/authorization/Authorization.tsx
+++ src/features/authorization/Authorization.tsx
@@ -1,7 +1,7 @@
 import { api } from 'app/api'
 import { useAppDispatch } from 'app/store/hooks'
 import { getUserTC } from 'app/store/userReducer'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { tg } from 'utils/tg'
 import { Loading } from '../loading/Loading'
@@ -11,16 +11,18 @@ export const Authorization = () => {
 
 	const dispatch = useAppDispatch()
 
+	const [description, setDescription] = useState<string>('')
+
 	useEffect(() => {
 		const urlId = window.Telegram.WebApp.initDataUnsafe.start_param
 		window.Telegram.WebApp.setHeaderColor('secondary_bg_color')
 		if (urlId) {
 			api.getUrl(urlId, tg.initDataUnsafe.user!.id).then(data => {
 				window.Telegram.WebApp.openTelegramLink(data.url)
-				setTimeout(() => window.Telegram.WebApp.close(), 10000)
+				setDescription(data.url)
 			})
 		} else dispatch(getUserTC(navigate))
 	}, [dispatch, navigate])
 
-	return <Loading />
+	return <Loading description={description} />
 }
